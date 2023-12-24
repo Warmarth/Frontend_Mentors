b41d56bbe2b26d54b4bba433ce146d1a6508c93c
Warmarth <godsonibeaka@gmail.com>
2023-12-24 13:25:14 +0100

    new files

---
+++ Newbie/faq-accordion-card-main/article.js
@@ -0,0 +1,19 @@
+const tagIds = ["tag0", "tag1", "tag2", "tag3", "tag4"];
+tagIds.forEach((tagId) => {
+  const tag = document.getElementById(tagId);
+  tag.style.display = "none";
+});
+
+span.st
+
+function handleClick(tagId, span) {
+  let tag = document.getElementById(tagId);
+  if (tag.style.display === "none") {
+    tag.style.display = "block";
+    span.classList.add("rotate");
+  } else {
+    tag.style.display = "none";
+    span.classList.remove("rotate");
+  }
+}
+
--- Newbie/faq-accordion-card-main/faq-accordion-card-main/article.js
+++
@@ -1,6 +0,0 @@
-const demo = document.querySelectorAll("article");
-const span = document.querySelector("span");
-
-function handleClick() {
-  Array.from("demo").forEach((ele) => span.classList.add("tag rotete"));
-}
